16212a7e1f11c97451ac3f8e4aed6665676ccdc6
Etienne Millon <me@emillon.org>
2013-08-12 22:25:56 +0200

    dump header

--- rgb.rs
+++ rgb.rs
@@ -1,15 +1,16 @@
 use std::io;
 use std::os;
+use std::str;
 
 type Mem = ~[u8];
 
-fn load_rom (file: &str, m: &Mem) -> Result<(), ~str> {
+fn load_rom (file: &str) -> Result<Mem, ~str> {
     let path = PosixPath(file);
     let r = io::read_whole_file(&path);
     match r {
-        Ok(t) => {
-            println(fmt!("Read %u bytes", t.len()));
-            return Ok(())
+        Ok(m) => {
+            println(fmt!("Read %u bytes", m.len()));
+            return Ok(m)
         }
         Err(s) => {
             return Err(s)
@@ -25,6 +26,11 @@ fn write_mem (m: &mut Mem, n: u16, v: u8) {
     m[n] = v
 }
 
+fn dump_header (m: Mem) {
+    let title = m.slice(0x134, 0x143);
+    println(str::from_bytes(title))
+}
+
 fn main() {
     println("rgb");
     let args = os::args();
@@ -34,9 +40,10 @@ fn main() {
         return
     }
     let file = os::args()[1];
-    let mem = ~[];
-    match load_rom(file, &mem) {
-        Ok(()) => {}
+    match load_rom(file) {
+        Ok(mem) => {
+            dump_header(mem)
+        }
         Err(s) => {
             println(fmt!("Cannot read %s: %s", file, s));
         }
