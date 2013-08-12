52c4c28acf5ad15e3f515ca7e83f2b4a6e7f24b9
Etienne Millon <me@emillon.org>
2013-08-12 22:10:28 +0200

    rom loader

--- rgb.rs
+++ rgb.rs
@@ -1,6 +1,30 @@
 use std::io;
 use std::os;
 
+type Mem = ~[u8];
+
+fn load_rom (file: &str, m: &Mem) -> Result<(), ~str> {
+    let path = PosixPath(file);
+    let r = io::read_whole_file(&path);
+    match r {
+        Ok(t) => {
+            println(fmt!("Read %u bytes", t.len()));
+            return Ok(())
+        }
+        Err(s) => {
+            return Err(s)
+        }
+    }
+}
+
+fn read_mem (m: &Mem, n: u16) -> u8 {
+    m[n]
+}
+
+fn write_mem (m: &mut Mem, n: u16, v: u8) {
+    m[n] = v
+}
+
 fn main() {
     println("rgb");
     let args = os::args();
@@ -10,16 +34,11 @@ fn main() {
         return
     }
     let file = os::args()[1];
-    let path = PosixPath(file);
-    let r = io::read_whole_file(&path);
-    match r {
-        Ok(t) => {
-            println(fmt!("Read %u bytes", t.len()))
-        }
+    let mem = ~[];
+    match load_rom(file, &mem) {
+        Ok(()) => {}
         Err(s) => {
             println(fmt!("Cannot read %s: %s", file, s));
-            os::set_exit_status(1);
-            return
         }
     }
 }
